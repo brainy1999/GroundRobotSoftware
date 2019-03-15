91449642458e0619d9a9d5564c0ad1c06e4ec011
Bernard Li <brainy1999@users.noreply.github.com>
2019-03-14 22:48:02 -0500

    First Commit :D
    L298 Driver Working - current commands Left, Right, Forward, and Turn. See documentation for more details.

---
+++ GroundRobotSoftware/RobotControlClass.h
@@ -0,0 +1,26 @@
+/*
+WORK IN PROGRESS - Easy (almost FLL style) commands for a robot control
+
+LI INDUSTRIAL TECHNOLOGIES
+Software Division
+RobotController SOFTWARE 1.1
+
+
+*/
+
+class RobotControlClass
+{
+ String robotname = "";
+ public: RobotControlClass(String enterrobotname)
+ {
+   enterrobotname = robotname;
+   Serial.println("Boot OK");
+   
+   
+ }
+ public: void driveforward(int drivetime, int travelspeed)
+ {
+  
+ }
+};
+
